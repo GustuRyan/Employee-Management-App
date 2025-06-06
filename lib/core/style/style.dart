01df6a9bb2c256bdb9295fab4fc2cce1d873c8e6
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-06-06 21:26:15 +0800

    add style.dart

---
+++ lib/core/style/style.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+//Icon Size
+double arrowBackSize = 30;
+
+// Padding
+double paddingXs = 10;
+double paddingSm = 15;
+double paddingMd = 20;
+double paddingLg = 25;
+double paddingXl = 30;
+double paddingXXL = 100;
+double paddingXXXL = 180;
+
+double paddingDefault = paddingSm;
+
+// Margin
+double marginXs = 10;
+double marginSm = 15;
+double marginMd = 20;
+double marginLg = 25;
+double marginXl = 30;
+double marginDefault = paddingSm;
+
+// Border radius
+BorderRadius borderRadiusXs = BorderRadius.circular(10);
+BorderRadius borderRadiusSm = BorderRadius.circular(15);
+BorderRadius borderRadiusMd = BorderRadius.circular(20);
+BorderRadius borderRadiusLg = BorderRadius.circular(25);
+BorderRadius borderRadiusXl = BorderRadius.circular(30);
+BorderRadius borderRadiusDefault = borderRadiusSm;
+
+// Colors
+// Base
+Color colorPrimary = const Color(0xffF9FAFB);
+Color colorAccent = const Color(0xff08A6EA);
+Color colorSuccess = const Color(0xff28C76F);
+Color colorError = const Color(0xffFF4F61);
+Color colorWarning = const Color(0xffFF9500);
+Color colorCaution = const Color(0xffFFCC00);
+Color colorGreyDark = const Color(0xff747474);
+Color colorGrey = const Color(0xffB9B9B9);
+Color colorGreyLight = const Color(0xffF2F2F2);
+Color colorBlack = const Color(0xff000000);
+Color colorWhite = const Color(0xffffffff);
+
+// Additional
+Color colorTheme = const Color(0xFF2F80ED);
+Color colorCheckbox = const Color(0xff263B61);
+Color colorShadow = const Color(0xffebebeb);
+Color colorDisabled = const Color(0xffDCDCDC);
+Color colorButton = const Color(0xFF2F80ED);
+Color colorBackground = const Color(0xffF7F7F7);
+Color colorLine = const Color(0xffE3E3E3);
+
