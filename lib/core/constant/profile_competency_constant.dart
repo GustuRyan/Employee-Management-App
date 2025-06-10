5c45af6cafc59c02ac92d03fb2cfae8b95890459
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-06-10 21:22:13 +0800

    create constant profile competency

---
+++ lib/core/constant/profile_competency_constant.dart
@@ -0,0 +1,18 @@
+import 'package:hr_360/data/model/profile/competency/profile_competency_model.dart';
+
+final List<ProfileCompetencyModel> dataProfileCompetencyConstant = [
+  ProfileCompetencyModel(
+    id: 1,
+    jobType: 'Flutter',
+    levelExperience: 'Advance',
+    date: '2025-01-01',
+    description: 'Mobile Developer for Android & iOS',
+  ),
+  ProfileCompetencyModel(
+    id: 1,
+    jobType: 'Laravel Framework',
+    levelExperience: 'Advance',
+    date: '2025-01-01',
+    description: 'Fullstack Developer',
+  ),
+];
