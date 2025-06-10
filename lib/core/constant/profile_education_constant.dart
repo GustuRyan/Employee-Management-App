546543aadd6182c85ca365c7ef179b097987cb14
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-06-10 21:22:34 +0800

    create constant profile education

---
+++ lib/core/constant/profile_education_constant.dart
@@ -0,0 +1,12 @@
+import 'package:hr_360/data/model/profile/education/profile_education_model.dart';
+
+final List<ProfileEducationModel> dataProfileEducationConstant = [
+  ProfileEducationModel(
+    id: 1,
+    educationType: 'College / University',
+    nameCollage: 'ITB STIKOM Bali',
+    department: 'Sistem Informasi',
+    startDate: '2014-09-28',
+    finishDate: '2018-05-15',
+  ),
+];
