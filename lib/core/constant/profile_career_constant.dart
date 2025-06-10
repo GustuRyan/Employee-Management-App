d8f6321d96fc0b2222d329d6b4160007218e0acb
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-06-10 21:22:04 +0800

    create constant profile career

---
+++ lib/core/constant/profile_career_constant.dart
@@ -0,0 +1,12 @@
+import 'package:hr_360/data/model/profile/career/profile_career_model.dart';
+
+final List<ProfileCareerModel> dataProfileCareerConstant = [
+  ProfileCareerModel(
+    id: 1,
+    careerType: 'Mobile Developer',
+    department: 'Information Technology',
+    division: 'Software Development',
+    startDate: '2018-05-15',
+    finishDate: '2024-01-01',
+  ),
+];
