857fccef98cce0901220605a0d36e3c9b3a0a590
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-06-10 21:21:33 +0800

    create constant card data training

---
+++ lib/core/constant/card_data_trening_constant.dart
@@ -0,0 +1,60 @@
+import 'package:hr_360/core/utils/asset_util.dart';
+import 'package:hr_360/data/model/card/card_model.dart';
+import 'package:hr_360/data/model/card/card_trening_model.dart';
+import 'package:hr_360/data/model/profile/family/profile_family_model.dart';
+import 'package:hr_360/presentation/module/menu/traning/widget/card_training.dart';
+
+final List<CardTreningModel> cardDataTreningConstant = [
+  CardTreningModel(
+    id: 1,
+    tittle: "Software Development",
+    date: "07 - 02 - 2020",
+    time: "17 : 58",
+    address:
+        "Jl Tukad Yeh Ho 36/2 Jadi Anyar, Kec. kediri, Kab. Tabanan, Bali, 821345",
+    startwork: "Wed, 29 juni 1994",
+    endwork: "Wed, 29 juni 1994",
+    status: "active",
+    timework: "08:30 - 11:00",
+    profile: "assets/icon/icon-profile.png",
+  ),
+  CardTreningModel(
+    id: 2,
+    tittle: "Software Development",
+    date: "07 - 02 - 2020",
+    time: "17 : 58",
+    address:
+        "Jl Tukad Yeh Ho 36/2 Jadi Anyar, Kec. kediri, Kab. Tabanan, Bali, 821345",
+    startwork: "Wed, 29 juni 1994",
+    endwork: "Wed, 29 juni 1994",
+    status: "active",
+    timework: "08:30 - 11:00",
+    profile: "assets/icon/icon-profile.png",
+  ),
+  CardTreningModel(
+    id: 2,
+    tittle: "Software Development",
+    date: "07 - 02 - 2020",
+    time: "17 : 58",
+    address:
+        "Jl Tukad Yeh Ho 36/2 Jadi Anyar, Kec. kediri, Kab. Tabanan, Bali, 821345",
+    startwork: "Wed, 29 juni 1994",
+    endwork: "Wed, 29 juni 1994",
+    status: "active",
+    timework: "08:30 - 11:00",
+    profile: ["assets/icon/icon-profile.png", "assets/icon/icon-profile.png"],
+  ),
+    CardTreningModel(
+    id: 3,
+    tittle: "Software Development",
+    date: "07 - 02 - 2020",
+    time: "17 : 58",
+    address:
+        "Jl Tukad Yeh Ho 36/2 Jadi Anyar, Kec. kediri, Kab. Tabanan, Bali, 821345",
+    startwork: "Wed, 29 juni 1994",
+    endwork: "Wed, 29 juni 1994",
+    status: "Completed",
+    timework: "08:30 - 11:00",
+    profile: ["assets/icon/icon-profile.png", "assets/icon/icon-profile.png"],
+  ),
+];
