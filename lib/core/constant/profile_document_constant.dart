cbfcaf4450637e14a0e2bc81d40a9885458d965a
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-06-10 21:22:25 +0800

    create constant profile document

---
+++ lib/core/constant/profile_document_constant.dart
@@ -0,0 +1,10 @@
+import 'package:hr_360/data/model/profile/document/profile_document_model.dart';
+
+final List<ProfileDocumentModel> dataProfileDocumentConstant = [
+  ProfileDocumentModel(
+    id: 1,
+    name: 'Ijazah',
+    description: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.',
+    path: '',
+  ),
+];
