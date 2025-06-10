9c3d9d8ab04bb4e07d7d7b72899dd77f65d90223
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-06-10 21:21:24 +0800

    create constant card data

---
+++ lib/core/constant/card_data_constant.dart
@@ -0,0 +1,58 @@
+import 'package:hr_360/core/utils/asset_util.dart';
+import 'package:hr_360/data/model/card/card_model.dart';
+import 'package:hr_360/data/model/profile/family/profile_family_model.dart';
+
+final List<CardModel> cardDataConstant = [
+  CardModel(
+    id: 1,
+    tittle: "Checking document",
+    date: "07 - 02 - 2020",
+    time: "17 : 58",
+    address:
+        "Jl Tukad Yeh Ho 36/2 Jadi Anyar, Kec. kediri, Kab. Tabanan, Bali, 821345",
+    startwork: "Wed, 29 juni 1994",
+    endwork: "Wed, 29 juni 1994",
+    status: "pending",
+    timework: "08:30 - 11:00",
+    profile: "assets/icon/icon-profile.png",
+  ),
+  CardModel(
+    id: 2,
+    tittle: "Checking document",
+    date: "07 - 02 - 2020",
+    time: "17 : 58",
+    address:
+        "Jl Tukad Yeh Ho 36/2 Jadi Anyar, Kec. kediri, Kab. Tabanan, Bali, 821345",
+    startwork: "Wed, 29 juni 1994",
+    endwork: "Wed, 29 juni 1994",
+    status: "active",
+    timework: "08:30 - 11:00",
+    profile: "assets/icon/icon-profile.png",
+  ),
+  CardModel(
+    id: 2,
+    tittle: "Checking document",
+    date: "07 - 02 - 2020",
+    time: "17 : 58",
+    address:
+        "Jl Tukad Yeh Ho 36/2 Jadi Anyar, Kec. kediri, Kab. Tabanan, Bali, 821345",
+    startwork: "Wed, 29 juni 1994",
+    endwork: "Wed, 29 juni 1994",
+    status: "rejected",
+    timework: "08:30 - 11:00",
+    profile: ["assets/icon/icon-profile.png", "assets/icon/icon-profile.png"],
+  ),
+    CardModel(
+    id: 3,
+    tittle: "Checking document",
+    date: "07 - 02 - 2020",
+    time: "17 : 58",
+    address:
+        "Jl Tukad Yeh Ho 36/2 Jadi Anyar, Kec. kediri, Kab. Tabanan, Bali, 821345",
+    startwork: "Wed, 29 juni 1994",
+    endwork: "Wed, 29 juni 1994",
+    status: "Approved",
+    timework: "08:30 - 11:00",
+    profile: ["assets/icon/icon-profile.png", "assets/icon/icon-profile.png"],
+  ),
+];
