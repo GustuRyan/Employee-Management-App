8b06430d47697f0abe915d4dfa73de727efc0c35
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-06-13 22:42:10 +0800

    create card model

---
+++ lib/data/model/card/card_model.dart
@@ -0,0 +1,25 @@
+class CardModel {
+  dynamic id;
+  dynamic tittle;
+  dynamic date;
+  dynamic time;
+  dynamic address;
+  dynamic startwork;
+  dynamic endwork;
+  dynamic status;
+  dynamic timework;
+  dynamic profile;
+
+  CardModel({
+    this.id,
+    this.tittle,
+    this.date,
+    this.time,
+    this.address,
+    this.startwork,
+    this.endwork,
+    this.status,
+    this.timework,
+    this.profile,
+  });
+}
