bcfcfe9991c2fffa317f8350e5487dbbc4b500bd
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-06-21 22:44:33 +0800

    create profile_document_model

---
+++ lib/data/model/profile/document/profile_document_model.dart
@@ -0,0 +1,8 @@
+class ProfileDocumentModel {
+  dynamic id;
+  dynamic name;
+  dynamic path;
+  dynamic description;
+
+  ProfileDocumentModel({this.id, this.name, this.path, this.description});
+}
