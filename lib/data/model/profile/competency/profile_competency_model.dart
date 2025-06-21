976076460a97c86b0ca80ecda69f47e2202f79e2
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-06-21 22:44:24 +0800

    create profile_competency_model

---
+++ lib/data/model/profile/competency/profile_competency_model.dart
@@ -0,0 +1,15 @@
+class ProfileCompetencyModel {
+  dynamic id;
+  dynamic jobType;
+  dynamic levelExperience;
+  dynamic date;
+  dynamic description;
+
+  ProfileCompetencyModel({
+    this.id,
+    this.jobType,
+    this.levelExperience,
+    this.date,
+    this.description,
+  });
+}
