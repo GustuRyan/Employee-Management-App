745628ed876fe7e2769281d9581a1072e9242539
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-06-21 22:43:40 +0800

    create performance_my_rank_model

---
+++ lib/data/model/performance/performance_my_rank_model.dart
@@ -0,0 +1,9 @@
+class PerformanceMyRankModel {
+  dynamic image;
+  dynamic points;
+  dynamic status;
+  dynamic order;
+  dynamic title;
+
+  PerformanceMyRankModel({this.image, this.points, this.status, this.order, this.title});
+}
