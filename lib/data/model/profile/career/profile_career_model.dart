eb1dcb4429bca335ece964dbf634b93cc70db8df
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-06-21 22:44:09 +0800

    create profile_career_model

---
+++ lib/data/model/profile/career/profile_career_model.dart
@@ -0,0 +1,17 @@
+class ProfileCareerModel {
+  dynamic id;
+  dynamic careerType;
+  dynamic department;
+  dynamic division;
+  dynamic startDate;
+  dynamic finishDate;
+
+  ProfileCareerModel({
+    this.id,
+    this.careerType,
+    this.department,
+    this.division,
+    this.startDate,
+    this.finishDate,
+  });
+}
