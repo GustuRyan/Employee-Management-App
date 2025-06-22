53f92da173a573cdffd9bb9d6656a42c3dfb34d2
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-06-22 23:32:42 +0800

    create profile_experience_model

---
+++ lib/data/model/profile/experience/profile_experience_model.dart
@@ -0,0 +1,19 @@
+class ProfileExperienceModel {
+  dynamic id;
+  dynamic experienceType;
+  dynamic name;
+  dynamic status;
+  dynamic picture;
+  dynamic startDate;
+  dynamic finishDate;
+
+  ProfileExperienceModel({
+    this.id,
+    this.experienceType,
+    this.name,
+    this.status,
+    this.picture,
+    this.startDate,
+    this.finishDate,
+  });
+}
