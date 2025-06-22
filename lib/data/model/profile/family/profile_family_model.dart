fc0dcaccca788aaca7e0df53b79904ef014f787c
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-06-22 23:32:50 +0800

    create profile_family_model

---
+++ lib/data/model/profile/family/profile_family_model.dart
@@ -0,0 +1,19 @@
+class ProfileFamilyModel {
+  dynamic id;
+  dynamic familyType;
+  dynamic name;
+  dynamic gender;
+  dynamic birthdate;
+  dynamic address;
+  dynamic picture;
+
+  ProfileFamilyModel({
+    this.id,
+    this.familyType,
+    this.name,
+    this.gender,
+    this.birthdate,
+    this.address,
+    this.picture,
+  });
+}
