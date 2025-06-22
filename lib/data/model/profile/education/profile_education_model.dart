6029998df2db6ea7b91ccae07d305a36a70cf903
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-06-22 23:32:18 +0800

    create profile_education_model

---
+++ lib/data/model/profile/education/profile_education_model.dart
@@ -0,0 +1,17 @@
+class ProfileEducationModel {
+  dynamic id;
+  dynamic educationType;
+  dynamic nameCollage;
+  dynamic department;
+  dynamic startDate;
+  dynamic finishDate;
+
+  ProfileEducationModel({
+    this.id,
+    this.educationType,
+    this.nameCollage,
+    this.department,
+    this.startDate,
+    this.finishDate,
+  });
+}
