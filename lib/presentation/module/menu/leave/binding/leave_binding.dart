11d872a37097b5ec4b6fe0012a1cc198e39d538e
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-07-16 17:37:01 +0800

    create ui module leave binding

---
+++ lib/presentation/module/menu/leave/binding/leave_binding.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+import '../controller/leave_controller.dart';
+
+class LeaveBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.put(LeaveController());
+  }
+}
