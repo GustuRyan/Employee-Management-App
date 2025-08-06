0f1aacdf760a9c9566179e427ebfce86da0a3bc2
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-08-06 22:35:34 +0800

    create new ui approved page

---
+++ lib/presentation/module/menu/performance/page/approval/approved_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:hr_360/core/style/style.dart';
+import 'package:hr_360/presentation/module/menu/performance/controller/performance_controller.dart';
+import 'package:hr_360/presentation/module/menu/performance/widget/performance_card.dart';
+
+class ApprovedPage extends GetResponsiveView<PerformanceController> {
+  ApprovedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(paddingMd),
+      child: Column(
+        children: List.generate(4, (index) {
+          return PerformanceCard(status: 'Completed', id: index,);
+        }),
+      ),
+    );
+  }
+}
