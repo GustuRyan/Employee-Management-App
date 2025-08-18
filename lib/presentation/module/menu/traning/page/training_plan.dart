db0fb5328fc57274da8ea52bccebbcfd607d8ea8
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-08-18 22:40:12 +0800

    create new ui training plan page

---
+++ lib/presentation/module/menu/traning/page/training_plan.dart
@@ -0,0 +1,45 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hr_360/core/constant/card_data_constant.dart';
+import 'package:hr_360/core/constant/card_data_trening_constant.dart';
+import 'package:hr_360/core/constant/profile_family_constant.dart';
+import 'package:hr_360/core/style/style.dart';
+import 'package:hr_360/core/utils/asset_util.dart';
+import 'package:hr_360/presentation/module/menu/overtime/controller/overtime_controller.dart';
+import 'package:hr_360/presentation/module/menu/overtime/widget/card_overtime.dart';
+import 'package:hr_360/presentation/module/menu/overtime/widget/overtime_graphics.dart';
+import 'package:hr_360/presentation/module/menu/traning/controller/training_controller.dart';
+import 'package:hr_360/presentation/module/menu/traning/widget/card_training.dart';
+import 'package:hr_360/presentation/widget/customs/custom_stacking_hero_section.dart';
+// import 'package:hr_360/presentation/module/profile/profile/data.dart';
+
+import '../../../../../../core/config/routes.dart';
+
+class TrainingPlan extends GetResponsiveView<TrainingController> {
+  TrainingPlan({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Column(
+          children:
+              cardDataTreningConstant
+                  .where(
+                    (item) => item.status.toString().toLowerCase() == 'active',
+                  )
+                  .map(
+                    (item) => Container(
+                      margin: EdgeInsets.symmetric(horizontal: marginMd),
+                      child: CardTraining(data: item),
+                    ),
+                  )
+                  .toList(),
+        ),
+      ],
+    );
+  }
+}
