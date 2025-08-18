cc8c4e7f589498d9805f64fc5b2779ba2978de0a
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-08-18 22:38:52 +0800

    create new ui training add attendance controller

---
+++ lib/presentation/module/menu/traning/controller/training_attendance_add_controller.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class TrainingAttendanceAddController extends GetxController {
+  RxList<int> activedCard = <int>[].obs;
+
+  String shortenText(String text, int maxLength) {
+    if (text.length <= maxLength) {
+      return text;
+    } else {
+      return '${text.substring(0, maxLength)}...';
+    }
+  }
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+}
