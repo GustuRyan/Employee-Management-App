182cf3bd1762c7a59a4f00f03919784c3d554685
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-08-18 22:39:06 +0800

    create new ui training controller

---
+++ lib/presentation/module/menu/traning/controller/training_controller.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:hr_360/core/style/style.dart';
+import 'package:hr_360/presentation/controller/permission_controller.dart';
+
+class TrainingController extends GetxController {
+  var currentIndex = 0.obs;
+
+  final List<String> navItems = ["Active", "History", "Training plan", "Activity"];
+
+  void thisCurrentIndex(int index) {
+    currentIndex.value = index;
+  }
+
+  bool isSelected(int index) {
+    return currentIndex.value == index;
+  }
+
+  // data color
+  Color getStatusColor(String status) {
+    switch (status) {
+      case 'active':
+      case 'Active':
+        return Colors.green;
+      case 'Completed':
+      case 'completed':
+        return Colors.blue;
+      default:
+        return Colors.grey;
+    }
+  }
+
+  // data static dropdown
+  var selectedValue = Rx<String?>(null);
+  final List<String> items = ['2025-04-01', '2025-04-02', '2025-04-03'];
+  // profile add tim overtime
+  final List<String> people = [
+    'John Doe',
+    'Jane Smith',
+    'Michael Johnson',
+    'Emily Davis',
+    'putra putra putra',
+    'putra putra putri',
+  ];
+
+  var selectedPeople = <String>[].obs;
+
+  void showPersonPicker(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Select a person'),
+          content: Container(
+            width: double.maxFinite,
+            child: ListView.builder(
+              itemCount: people.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(people[index]),
+                  onTap: () {
+                    if (!selectedPeople.contains(people[index])) {
+                      selectedPeople.add(people[index]);
+                    }
+                    Navigator.pop(context);
+                  },
+                );
+              },
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  void onInit() {
+    super.onInit();
+    permissionController.checkPermissionRequest();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+
+  GetStorage storage = GetStorage('Dimata');
+
+  final GlobalKey<ScaffoldState> scaffoldDashboardKey =
+      GlobalKey<ScaffoldState>();
+
+  final RxInt currentNavbar = 0.obs;
+
+  //
+  // final AlertController alertController = Get.put(AlertController());
+
+  final PermissionController permissionController = Get.put(
+    PermissionController(),
+  );
+}
