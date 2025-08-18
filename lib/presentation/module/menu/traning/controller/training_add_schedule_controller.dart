e5e3210dcba716d89b9a849c02eb1f77279bbb45
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-08-18 22:38:41 +0800

    create new ui training add schedule controller

---
+++ lib/presentation/module/menu/traning/controller/training_add_schedule_controller.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class TrainingAddScheduleController extends GetxController {
+  late Rxn<DateTime> fromDate = Rxn<DateTime>(null);
+  late Rxn<DateTime> toDate = Rxn<DateTime>(null);
+
+  late Rxn<TimeOfDay> fromTime = Rxn<TimeOfDay>(null);
+  late Rxn<TimeOfDay> toTime = Rxn<TimeOfDay>(null);
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+}
